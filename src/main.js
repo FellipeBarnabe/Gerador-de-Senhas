1fbe635fde5935e17f03d93854d07059d0d7bd7b
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2023-09-14 15:49:27 -0300

    firstCommit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { nanoid } from "nanoid";
 import './style.css'
+import copy from 'clipboard-copy';
 
 const passawordBtnEl = document.querySelector('button')
 const displayPassawordEl = document.querySelector('h2')
@@ -8,3 +9,8 @@ passawordBtnEl.addEventListener('click',() =>{
     const randomPassword = nanoid()
     displayPassawordEl.innerHTML = randomPassword
 })
+
+displayPassawordEl.addEventListener('click',(event) =>{
+    copy(event.target.innerHTML);
+    alert('Senha copiada!');
+});
